1d91ef1b7412c2f5460d76f520913906e5c74a31
Priyanshu Varshney <priyanshuvarshney027@gmail.com>
2026-01-23 22:43:49 +0530

    task1

--- Classwork/task1.js
+++ Classwork/task1.js
@@ -1,21 +1,50 @@
-const http = require('http');
 const fs = require('fs');
+const path = require('path');
 
-fs.writeFile("./task1.txt", "GLA University", 'utf-8', () => {});
-const res = fs.readFile("./task1.js", "utf-8", (err, result) => {
-    if(err){
-        console.log("Error",err);
-        
-    }
-    else{
-        console.log(res);
-    }
-});
-// const arg = process.argv;
-// const port = arg[2]
-// http.createServer((req, resp) => {
-    
-// }).listen(port)
+const cmd = process.argv[2];
+const filePath = process.argv[3];
+const content = process.argv[4];
 
-// console.log(fs.statSync("./task1.txt"))
-// console.log(fs.statSync("./task1.txt").isFile())
+function errorHandling(err) {
+    if (err.code === 'ENOENT') {
+        console.log("File not found");
+    } else if (err.code === 'EACCES') {
+        console.log("Permission Denied")
+    } else {
+        console.log("Error Occurred")
+    }
+}
+if (cmd === 'read') {
+    fs.readFile(filePath, "utf-8", (err, data) => {
+        if (err) {
+            console.log(errorHandling(err));
+        }
+        console.log(data)
+    });
+}
+else if (cmd === 'write') {
+    fs.writeFile(filePath, content, 'utf-8', (err, data) => {
+        if (err) console.log(errorHandling(err));
+        console.log(data);
+    });
+}
+
+else if (cmd === 'append') {
+    fs.appendFile(filePath, "\n"+content + "\n", (err) => {
+        if (err) console.log(errorHandling(err));
+        console.log("Content append Successfully");
+    });
+}
+
+else if (cmd === 'copy') {
+    fs.copyFile(filePath, "./copyFile.txt");
+}
+else if (cmd === 'copy') {
+    fs.unlink("./copy.File.txt");
+}
+else if (cmd === 'dir') {
+    fs.readdir("./", (err, files) => {
+        if (err) console.log(errorHandling(err));
+        console.log(files);
+    })
+}
