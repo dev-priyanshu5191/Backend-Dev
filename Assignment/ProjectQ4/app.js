d7ee4ca99be4f22426b559ecb7ec14d6756ab105
Priyanshu Varshney <priyanshuvarshney027@gmail.com>
2026-02-13 23:05:47 +0530

    assignment

---
+++ Assignment/ProjectQ4/app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+
+app.set("view engine", "ejs");
+
+
+app.get("/", (req, res) => {
+    res.send("Home Page");
+});
+
+
+app.use((req, res) => {
+    res.status(404).render("404");
+});
+
+
+app.listen(3300, () => {
+    console.log("Server running on port 3000");
+});
