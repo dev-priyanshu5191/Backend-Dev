18f7925cceef37a6430e95606701751676f171e8
Priyanshu Varshney <priyanshuvarshney027@gmail.com>
2026-02-16 14:28:52 +0530

    project

---
+++ Project/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const users = require("./MOCK_DATA.json");
+const app = express();
+app.get("/api/users", (req, resp) => {
+    const html = `<ul>
+                ${users.map((user) `<li> ${user.first_name} ${user.last_name}</li>`).join("")}
+                </ul>`;
+                resp.send(html);
+});
+// RESTFul API
+
+// GET Method
+app.get("/users", (req, resp) => {
+    resp.json({message: "All Users", users});
+});
+
+app.get("/api/users", (req, resp) => {
+    resp.json(users);
+});
+
+app.get((req, resp) => {
+    const id = req.params.id;
+    const user = user.find((u) => u.id == id);
+    return resp.json(user);
+})
+
+app.listen(5200, () => {
+    console.log("Server Started");
+})
